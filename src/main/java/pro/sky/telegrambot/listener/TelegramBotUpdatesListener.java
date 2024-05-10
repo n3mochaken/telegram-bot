10b5437264cab094c4cd9ef3914f354177da8180
n3mochaken <ns.mixa@gmail.com>
2024-05-10 22:25:03 +0300

    v1

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -34,40 +34,40 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
-    @Override
-    public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            if (update.message() != null && update.message().text() != null) {
-                if (update.message().text().equals("/start")) {
-                    long chatId = update.message().chat().id();
-                    telegramBot.execute(new SendMessage(chatId, "Привет! Я ТГ бот."));
-                } else {
-                    processIncomingMessage(update.message().chat().id(), update.message().text());
-                }
-            }
-        });
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
-    }
+//    @Override
+//    public int process(List<Update> updates) {
+//        updates.forEach(update -> {
+//            logger.info("Processing update: {}", update);
+//            if (update.message() != null && update.message().text() != null) {
+//                if (update.message().text().equals("/start")) {
+//                    long chatId = update.message().chat().id();
+//                    telegramBot.execute(new SendMessage(chatId, "Привет! Я ТГ бот."));
+//                } else {
+//                    processIncomingMessage(update.message().chat().id(), update.message().text());
+//                }
+//            }
+//        });
+//        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+//    }
 
-    public void processIncomingMessage(Long chatId, String message) {
-        Pattern pattern = Pattern.compile("([0-9.:/\\s]{16})(\\s)([\\W]+)");
-        Matcher matcher = pattern.matcher(message);
-        if (matcher.find()) {
-            String dateTimeString = matcher.group(1);
-            String reminderText = matcher.group(3);
-
-            LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-
-            NotificationTask notificationTask = new NotificationTask();
-            notificationTask.setChatId(chatId);
-            notificationTask.setNotificationDateTime(dateTime);
-            notificationTask.setNotificationText(reminderText);
-
-            notificationTaskRepository.save(notificationTask);
-        } else {
-            System.out.println("cheto ne poshlo");
-        }
-    }
+//    public void processIncomingMessage(Long chatId, String message) {
+//        Pattern pattern = Pattern.compile("([0-9.:/\\s]{16})(\\s)([\\W]+)");
+//        Matcher matcher = pattern.matcher(message);
+//        if (matcher.find()) {
+//            String dateTimeString = matcher.group(1);
+//            String reminderText = matcher.group(3);
+//
+//            LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+//
+//            NotificationTask notificationTask = new NotificationTask();
+//            notificationTask.setChatId(chatId);
+//            notificationTask.setNotificationDateTime(dateTime);
+//            notificationTask.setNotificationText(reminderText);
+//
+//            notificationTaskRepository.save(notificationTask);
+//        } else {
+//            System.out.println("cheto ne poshlo");
+//        }
+//    }
 }
 
