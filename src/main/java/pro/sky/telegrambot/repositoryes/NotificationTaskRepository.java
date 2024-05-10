7db80d5e4bc16b813b07f0588c2267d37f945a3f
n3mochaken <ns.mixa@gmail.com>
2024-05-10 22:45:45 +0300

    v1.4 ><

--- src/main/java/pro/sky/telegrambot/repositoryes/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repositoryes/NotificationTaskRepository.java
@@ -1,10 +1,14 @@
-//package pro.sky.telegrambot.repositoryes;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import pro.sky.telegrambot.model.NotificationTask;
-//
-//@Repository
-//public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
-//
-//}
+package pro.sky.telegrambot.repositoryes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.sky.telegrambot.model.NotificationTask;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+
+    List<NotificationTask> findByNotificationDateTime(LocalDateTime currentMinute);
+}
